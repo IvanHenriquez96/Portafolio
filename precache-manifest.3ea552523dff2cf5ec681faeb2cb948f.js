dd51e2512e448b34fadf203705b3bc0b924fcf6e
Ivan <iv.henriquez.96@gmail.com>
2020-04-09 02:27:37 -0300

    Updates

---
+++ precache-manifest.3ea552523dff2cf5ec681faeb2cb948f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d03ed39c15569af7d794c12b9b2fc6",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "2a9d611c76c0d4ed9595",
+    "url": "/Portafolio/static/css/main.486b8e57.chunk.css"
+  },
+  {
+    "revision": "60c4fe41a1324fa6f0f4",
+    "url": "/Portafolio/static/js/2.8e731ddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portafolio/static/js/2.8e731ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a9d611c76c0d4ed9595",
+    "url": "/Portafolio/static/js/main.9050eacf.chunk.js"
+  },
+  {
+    "revision": "8ad9e88dc52db5b968e9",
+    "url": "/Portafolio/static/js/runtime-main.7d0c046c.js"
+  }
+]);
